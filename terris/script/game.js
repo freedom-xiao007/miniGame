d73ad34ffa60802b3d542a3606813bd9247e5e36
萧 <1243925457@qq.com>
2022-07-10 10:28:58 +0800

    方块随机生成

--- terris/script/game.js
+++ terris/script/game.js
@@ -1,88 +1,172 @@
-var screen = document.getElementById('game');
+var screen = document.getElementById("game");
 console.log(screen);
-var brush = screen.getContext('2d');
-brush.clearRect(0, 0, 300, 400);
+var brush = screen.getContext("2d");
+
+setInterval("gameCycle()", 1000);
+
+const BLOCK_TYPE = {
+  "I" : 1,
+  "LEFT_L": 2,
+  "RIGHT_L": 3,
+  "O": 4,
+  "LEFT_S": 5,
+  "RIGHT_S": 6,
+  "T": 7,
+};
+const BLOCK_WIDTH = 10;
+const BLOCK_HEIGHT = 10;
+
+function gameCycle() {
+  brush.clearRect(0, 0, 150, 400);
+  createBlock();
+  moveBlock();
+}
+
+function createBlock() {
+  var randomType = Math.round(Math.random() * 6 + 1);
+  console.log(randomType);
+  switch (randomType) {
+	case BLOCK_TYPE.I:
+		createI(10, 70);
+		break;
+	case BLOCK_TYPE.LEFT_L:
+		crateLeftL(10, 70);
+		break;
+	case BLOCK_TYPE.RIGHT_L:
+		createRightL(10, 70);
+		break;
+	case BLOCK_TYPE.O:
+		createO(10, 70);
+		break;
+	case BLOCK_TYPE.LEFT_S:
+		createLeftS(10, 70);
+		break;
+	case BLOCK_TYPE.RIGHT_S:
+		createRightS(10, 70);
+		break;
+	case BLOCK_TYPE.T:
+		createT(10, 70);
+		break;
+  }
+}
+
+function moveBlock() {}
+
+// createI(10, 10);
+// crateLeftL(30, 10);
+// createRightL(70, 20);
+// createO(110, 10);
+// createLeftS(10, 70);
+// createRightS(50, 60);
+// createT(90, 70);
+
+function drawSolid(x1, y1, x2, y2, x3, y3, x4, y4, color) {
+	brush.fillStyle = color;
+	brush.fillRect(x1, y1, BLOCK_WIDTH, BLOCK_HEIGHT);
+	brush.fillRect(x2, y2, BLOCK_WIDTH, BLOCK_HEIGHT);
+	brush.fillRect(x3, y3, BLOCK_WIDTH, BLOCK_HEIGHT);
+	brush.fillRect(x4, y4, BLOCK_WIDTH, BLOCK_HEIGHT);
+	drawOutline(x1, y1, x2, y2, x3, y3, x4, y4);
+}
+
+function drawOutline(x1, y1, x2, y2, x3, y3, x4, y4) {
+	brush.fillStyle = "black";
+	brush.strokeRect(x1, y1, BLOCK_WIDTH, BLOCK_HEIGHT);
+	brush.strokeRect(x2, y2, BLOCK_WIDTH, BLOCK_HEIGHT);
+	brush.strokeRect(x3, y3, BLOCK_WIDTH, BLOCK_HEIGHT);
+	brush.strokeRect(x4, y4, BLOCK_WIDTH, BLOCK_HEIGHT);
+}
 
 // I
-brush.fillStyle = 'cyan';
-brush.fillRect(10, 10, 10, 10);
-brush.fillRect(10, 20, 10, 10);
-brush.fillRect(10, 30, 10, 10);
-brush.fillRect(10, 40, 10, 10);
-brush.fillStyle = 'black';
-brush.strokeRect(10, 10, 10, 10);
-brush.strokeRect(10, 20, 10, 10);
-brush.strokeRect(10, 30, 10, 10);
-brush.strokeRect(10, 40, 10, 10);
+function createI(startX, startY) {
+	x1 = startX;
+	x2 = x1;
+	x3 = x1;
+	x4 = x1;
+	y1 = startY;
+	y2 = y1 + BLOCK_HEIGHT;
+	y3 = y2 + BLOCK_HEIGHT;
+	y4 = y3 + BLOCK_HEIGHT;
+	drawSolid(x1, y1, x2, y2, x3, y3, x4, y4, "cyan");
+}
 
 // 左L
-brush.fillStyle = 'blue';
-brush.fillRect(30, 10, 10, 10);
-brush.fillRect(30, 20, 10, 10);
-brush.fillRect(40, 20, 10, 10);
-brush.fillRect(50, 20, 10, 10);
-brush.fillStyle = 'black';
-brush.strokeRect(30, 10, 10, 10);
-brush.strokeRect(30, 20, 10, 10);
-brush.strokeRect(40, 20, 10, 10);
-brush.strokeRect(50, 20, 10, 10);
+function crateLeftL(startX, startY) {
+	x1 = startX;
+	x2 = x1;
+	x3 = x2 + BLOCK_WIDTH;
+	x4 = x3 + BLOCK_WIDTH;
+	y1 = startY;
+	y2 = y1 + BLOCK_HEIGHT;
+	y3 = y2;
+	y4 = y2;
+	drawSolid(x1, y1, x2, y2, x3, y3, x4, y4, "blue");
+}
 
 // 右L
-brush.fillStyle = 'orange';
-brush.fillRect(70, 20, 10, 10);
-brush.fillRect(80, 20, 10, 10);
-brush.fillRect(90, 20, 10, 10);
-brush.fillRect(90, 10, 10, 10);
-brush.fillStyle = 'black';
-brush.strokeRect(70, 20, 10, 10);
-brush.strokeRect(80, 20, 10, 10);
-brush.strokeRect(90, 20, 10, 10);
-brush.strokeRect(90, 10, 10, 10);
+function createRightL(startX, startY) {
+	x1 = startX;
+	x2 = x1 + BLOCK_HEIGHT;
+	x3 = x2 + BLOCK_WIDTH;
+	x4 = x3;
+	y1 = startY;
+	y2 = y1;
+	y3 = y2;
+	y4 = y3 - BLOCK_HEIGHT;
+	drawSolid(x1, y1, x2, y2, x3, y3, x4, y4, "orange");
+}
+
 
 // o
-brush.fillStyle = 'yellow';
-brush.fillRect(110, 10, 10, 10);
-brush.fillRect(110, 20, 10, 10);
-brush.fillRect(120, 10, 10, 10);
-brush.fillRect(120, 20, 10, 10);
-brush.fillStyle = 'black';
-brush.strokeRect(110, 10, 10, 10);
-brush.strokeRect(110, 20, 10, 10);
-brush.strokeRect(120, 10, 10, 10);
-brush.strokeRect(120, 20, 10, 10);
+function createO(startX, startY) {
+	x1 = startX;
+	x2 = x1,
+	x3 = x1 + BLOCK_WIDTH;
+	x4 = x3;
+	y1 = startY;
+	y2 = y1 + BLOCK_HEIGHT;
+	y3 = y1;
+	y4 = y3 + BLOCK_HEIGHT;
+	drawSolid(x1, y1, x2, y2, x3, y3, x4, y4, "yellow");
+}
+
 
 // 左S
-brush.fillStyle = 'green';
-brush.fillRect(10, 70, 10, 10);
-brush.fillRect(20, 70, 10, 10);
-brush.fillRect(20, 60, 10, 10);
-brush.fillRect(30, 60, 10, 10);
-brush.fillStyle = 'black';
-brush.strokeRect(10, 70, 10, 10);
-brush.strokeRect(20, 70, 10, 10);
-brush.strokeRect(20, 60, 10, 10);
-brush.strokeRect(30, 60, 10, 10);
+function createLeftS(startX, startY) {
+	x1 = startX;
+	x2 = x1 + BLOCK_WIDTH,
+	x3 = x2;
+	x4 = x3 + BLOCK_WIDTH;
+	y1 = startY;
+	y2 = y1;
+	y3 = y2 - BLOCK_HEIGHT;
+	y4 = y3;
+	drawSolid(x1, y1, x2, y2, x3, y3, x4, y4, "green");
+}
+
 
 // 右S
-brush.fillStyle = 'green';
-brush.fillRect(50, 60, 10, 10);
-brush.fillRect(60, 60, 10, 10);
-brush.fillRect(60, 70, 10, 10);
-brush.fillRect(70, 70, 10, 10);
-brush.fillStyle = 'black';
-brush.strokeRect(50, 60, 10, 10);
-brush.strokeRect(60, 60, 10, 10);
-brush.strokeRect(60, 70, 10, 10);
-brush.strokeRect(70, 70, 10, 10);
+function createRightS(startX, startY) {
+	x1 = startX;
+	x2 = x1 + BLOCK_WIDTH,
+	x3 = x2;
+	x4 = x3 + BLOCK_WIDTH;
+	y1 = startY;
+	y2 = y1;
+	y3 = y2 + BLOCK_HEIGHT;
+	y4 = y3;
+	drawSolid(x1, y1, x2, y2, x3, y3, x4, y4, "purple");	
+}
 
 // T
-brush.fillStyle = 'red';
-brush.fillRect(90, 70, 10, 10);
-brush.fillRect(100, 70, 10, 10);
-brush.fillRect(100, 60, 10, 10);
-brush.fillRect(110, 70, 10, 10);
-brush.fillStyle = 'black';
-brush.strokeRect(90, 70, 10, 10);
-brush.strokeRect(100, 70, 10, 10);
-brush.strokeRect(100, 60, 10, 10);
-brush.strokeRect(110, 70, 10, 10);
+function createT(startX, startY) {
+	x1 = startX;
+	x2 = x1 + BLOCK_WIDTH,
+	x3 = x2;
+	x4 = x3 + BLOCK_WIDTH;
+	y1 = startY;
+	y2 = y1;
+	y3 = y2 - BLOCK_HEIGHT;
+	y4 = y1;
+	drawSolid(x1, y1, x2, y2, x3, y3, x4, y4, "red");	
+}	
